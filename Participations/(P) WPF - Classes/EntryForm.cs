9ad27f277e0774b7b49deb7fcff833b4d899a58b
lohm5697 <jason.c.lohmer@ou.edu>
2020-01-30 12:56:48 -0600

    WPF-Classes participation done

---
+++ Participations/(P) WPF - Classes/EntryForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _P__WPF___Classes
+{
+    class EntryForm
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int ZipCode { get; set; }
+
+        public EntryForm()
+        {
+            Name = string.Empty;
+            Address = string.Empty;
+            ZipCode = 0;
+        }
+
+        public EntryForm(string name, string address, int zipcode)
+        {
+            Name = name;
+            Address = address;
+            ZipCode = zipcode;
+        }
+
+        public override string ToString()
+        {
+            string info;
+            info = Name + " " + Address + " " + ZipCode;
+            return info;
+        }
+    }
+}
---
+++ Participations/(P) WPF - Classes/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _P__WPF___Classes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_sumbit_Click(object sender, RoutedEventArgs e)
+        {
+            EntryForm entry1 = new EntryForm();
+            entry1.Name = txt_name.Text;
+            entry1.Address = txt_address.Text;
+            entry1.ZipCode = Convert.ToInt32(txt_zipcode.Text);
+
+            
+
+            lst_submissions.Items.Add(entry1);
+
+
+            
+        }
+    }
+}
