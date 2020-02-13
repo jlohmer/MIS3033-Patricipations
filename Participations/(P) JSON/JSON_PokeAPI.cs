1fd285e0f36e164fd2cf2ef00a3469bf08c900cb
lohm5697 <jason.c.lohmer@ou.edu>
2020-02-13 13:15:58 -0600

    (P) JSON attempted

---
+++ Participations/(P) JSON/JSON_PokeAPI.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _P__JSON
+{
+    class AllPokemonAPI
+    {
+        public int count { get; set; }
+        public string next { get; set; }
+        public string previous { get; set; }
+        public List<AllPokemonResult> results { get; set; }
+    }
+
+    public class AllPokemonResult
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+    public class pokieInfo
+    {
+        public string name { get; set; }
+        public int weight { get; set; }
+        public int height { get; set; }
+        public string image { get; set; }
+
+        public override string ToString()
+        {
+            return image;
+        }
+    }
+}
---
+++ Participations/(P) JSON/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _P__JSON
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string path = "https://pokeapi.co/api/v2/pokemon?offset-0&limit=1000";
+
+            AllPokemonAPI pokemonAPI;
+            using (var client = new HttpClient())
+            {
+                string json = client.GetStringAsync(path).Result;
+
+                pokemonAPI = JsonConvert.DeserializeObject<AllPokemonAPI>(json);
+            }
+
+            foreach (var pokie in pokemonAPI.results)
+            {
+                lst_Pokemon.Items.Add(pokie);
+            }
+        }
+        private void lst_Pokemon_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedPokemonFromList = (pokieInfo)lst_Pokemon.SelectedItem;
+
+            img_Pokemon.Source = new BitmapImage(new Uri(selectedPokemonFromList.image));
+        }
+    }
+}
